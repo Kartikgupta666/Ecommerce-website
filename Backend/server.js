c14581f30bcf716160d4b7116d31654d6bb349ea
kartikgupta666 <kartikgangil@gmail.com>
2024-06-26 21:46:41 +0530

    make user schema + sign up endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const connectToDatabase = require('./db')
 const app = express();
 const port = 9000;
+const user = require('./User_schema')
 connectToDatabase();
 
 app.use(express.json());
@@ -11,6 +12,28 @@ app.use(express.json());
 
 app.get('/' , (req , res)=>{
      res.send("hello this is my new end point");
+})
+
+
+app.post('/signup'  , async(req , res)=>{
+    const {name , email , password} = req.body;
+const id = await user.findOne({email})
+if(id){
+    return res.send("user alredy exist")
+    // console.log(id)
+}
+else {
+    const data = {
+        name : name,
+        email:email,
+        password : password 
+    }
+
+    user.insertMany(data);
+    return res.send("data is successfully stored");
+}
+    
+
 })
 
 
